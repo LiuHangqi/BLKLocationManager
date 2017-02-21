250845ec451ff8b7cd699b0ff965b84d4fb01089
liu <liu@AC960F15.ipt.aol.com>
2017-02-21 14:56:17 +0800

    first commit

---
+++ Example/Pods/Target Support Files/BLKLocationManager/BLKLocationManager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BLKLocationManager : NSObject
+@end
+@implementation PodsDummy_BLKLocationManager
+@end
---
+++ Example/Pods/Target Support Files/BLKLocationManager/BLKLocationManager-umbrella.h
@@ -0,0 +1,9 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "BLKLocationManager.h"
+
+FOUNDATION_EXPORT double BLKLocationManagerVersionNumber;
+FOUNDATION_EXPORT const unsigned char BLKLocationManagerVersionString[];
+
